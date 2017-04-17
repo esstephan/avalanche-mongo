ff517dc652121b3d57516e8314faae665ce33781
esstephan <ericastephan@gmail.com>
2017-04-17 11:39:41 -0700

    moves most functionality from server.js to routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,2 +1,75 @@
 //server side routing
 
+// set up ========================
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+// var mongoose = require('mongoose');
+// var User = require('../db/db.js');
+
+// create our app object and tell it to use some middleware
+var app = express();
+app.use(express.static(path.join(process.cwd(), 'public')));
+app.use(bodyParser.urlencoded());
+
+// create dummy route responses
+app.post('/signup', function(req, res) {
+  res.send("You have signed up!");
+});
+
+app.get('/matches', function (req, res) {
+  res.send("Your matches would be sent here");
+})
+
+// var fake = new User;
+// fake.id = 1;
+// fake.name = "Ada";
+// fake.email = "ada@gmail.com";
+// fake.times = "7pm";
+
+// // //save fake user
+// fake.save(function(err){
+//   if (err){
+//     console.log("error saving test user");
+//     return (err);
+//   } else {
+//     console.log("user successfully saved");
+//   }
+// })
+
+
+// app.post('/api/users', function(req, res){
+//   var user = new User({ name: req.body.username, email: req.body.email, times: req.body.times });
+//     user.save(function (err, user) {
+//       if (err) {
+//         return console.error(err);
+//       } else {
+//         console.log ("successfully saved ", user);
+//         res.end(user);
+//     }
+//   });
+// });
+
+// var retrieveAllUsers = Q.nbind(User.find, User);
+
+// app.get('/api/users', function(req, res, next){
+//   retrieveAllUsers({})
+//   .then(function(retrieved){
+//     console.log("finding all users")
+//     res.json(retrieved);
+//   })
+//   .fail(function(error){
+//     res.end("error: " + error);
+//   });
+// })
+
+
+
+//app.post('/api/users', userController.signin)
+
+// Node.js export - make the app available to other files when 'required' =====
+
+module.exports = app;
+
+
