81de78329105e439cbcca423b26bd636fe01894c
esstephan <ericastephan@gmail.com>
2017-04-20 07:37:57 -0700

    fake data for testing

---
+++ server/fake.js
@@ -0,0 +1,23 @@
+var fake = [{
+  id: 1,
+  name: "Ada",
+  email: "ada@gmail.com",
+  times: {
+    1: 11,
+    2: 12,
+    3: 13
+  },
+},
+  {
+    id: 2,
+    name: "Bob",
+    email: "bob@gmail.com",
+    times: {
+      1: 13,
+      2: 11,
+      3: 15
+    },
+  }
+];
+
+module.exports = fake;
