657ec603bfca5900443a02c53a20ff9470705af5
esstephan <ericastephan@gmail.com>
2017-04-24 13:41:24 -0700

    move db connection to its own file

--- server/server.js
+++ server/server.js
@@ -1,10 +1,5 @@
-// set up ========================
-
 // require app object and its routing
 var app = require('./routes.js');
-
-// require mongoose to make MongoDB database connection
-var mongoose = require('mongoose');
 
 // listen (start app with node server.js) =========
 var port = process.env.PORT || 3000;
@@ -12,9 +7,6 @@ var port = process.env.PORT || 3000;
 app.listen(port, function(){
   console.log("App is running on port" + port);
 });
-
-// connect to database =================
-mongoose.connect('mongodb://pf_admin:phone4friend@ds057176.mlab.com:57176/phone-friend_users');
 
 
 
