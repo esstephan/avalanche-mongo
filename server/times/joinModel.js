2ebccf1b16361a560d17a0810243a946d5b6e302
esstephan <ericastephan@gmail.com>
2017-05-01 21:35:42 -0700

    move time information into user object (temporarily?)

--- server/times/joinModel.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var JoinSchema = new mongoose.Schema({
-  timeID: Number,
-  userID: Number,
-  matchMade: false
-});
-
-module.exports = mongoose.model('Join', JoinSchema);
--- server/times/timeModel.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var TimeSchema = new mongoose.Schema({
-  id: Number,
-  time: Number
-});
-
-module.exports = mongoose.model('Time', TimeSchema);
