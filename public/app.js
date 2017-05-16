50751543d612437d70db6ccd7b1551e64199f4b5
esstephan <ericastephan@gmail.com>
2017-05-16 10:20:20 -0700

    add route to make-a-call page

--- public/app.js
+++ public/app.js
@@ -5,13 +5,17 @@ var AppearIn = window.AppearIn;
 var appearin = new AppearIn;
 
 
-var app = angular.module('app', ['ngRoute', 'app.signup', 'app.match']);
+var app = angular.module('app', ['ngRoute', 'app.signup', 'app.match', 'app.signupwithform']);
 app.config(function($routeProvider, $locationProvider) {
     $locationProvider.hashPrefix('');
     $routeProvider
       .when('/signup', {
         templateUrl: 'signup.html',
         controller: 'SignUpController',
+      })
+      .when('/signupWithForm', {
+        templateUrl: 'signupWithForm.html',
+        controller: 'SignUpWithFormController',
       })
       .when('/match', {
         templateUrl: 'match.html',
