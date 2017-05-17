c26bd9a033785ee43825590fb56ed66811a1ef58
esstephan <ericastephan@gmail.com>
2017-05-16 17:15:39 -0700

    removed unneeded Account and Time models

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,31 +1,26 @@
 var User = require ('../models/User.js');
-var Time = require ('../models/Time.js');
-//ooooo
 
-module.exports = {
+  // createUser = function (req, res) {
+  //   console.log('req.body is', req.body);
+  //   var newUser = User(req.body);
+  //   newUser.save()
+  //   .then(function(savedUser) {
+  //     return (savedUser.name + ' created');
+  //   })
+  //   .catch(function(error) {
+  //     return ('error: ', error);
+  //   });
+  // }
 
-  createUser : function(req, res) {
-    console.log('req.body is', req.body)
-    var newUser=User(req.body);
-
-    newUser.save(req.body)
-    .then(function(savedUser) {
-      res.status(200).json(savedUser.name + ' created');
-    })
-    .catch(function(error) {
-      res.end('error: ', error);
-    });
-  },
-
-  findAll : function(req, res) {
-    User.find{}
-    .then(function(users) {
-      res.status(200).json(users);
-    })
-    .catch(function(error) {
-      res.end('error: ', error);
-    });
-  }
+  // findAll = function (req, res) {
+  //   User.find({})
+  //   .then(function(users) {
+  //     return users;
+  //   })
+  //   .catch(function(error) {
+  //     return ('error: ', error);
+  //   });
+  // }
 
   // findMatches: function(req, res) {
   //   User.find({ times: { "$in" : req.body.times} })
@@ -33,5 +28,3 @@ module.exports = {
 
   //   })
   // }
-
-}
--- server/models/Account.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-passportLocalMongoose = require('passport-local-mongoose');
-
-var Account = new Schema({
-  nickname: String,
-})
