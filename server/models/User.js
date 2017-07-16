eca505e4089e71a0b83c55ebd114b03afd43884a
esstephan <ericastephan@gmail.com>
2017-07-16 15:04:28 -0700

    changed user fieldnames

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,11 @@
 var mongoose = require('mongoose');
 var passportLocalMongoose = require('passport-local-mongoose');
+var jwt = require('jsonwebtoken');
 var Schema = mongoose.Schema;
 
 var User = new Schema({
-  displayname: String,
+  firstName: String,
+  lastName: String,
   email: String,
   username: String,
   notes: String,
@@ -16,6 +18,20 @@ var User = new Schema({
   }],
   timezone: Number,
 });
+
+User.methods.generateJWT = function() {
+  // set expiration date for 7 days from now
+  var expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7)
+
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    username: this.username,
+    exp: parseInt(expiry.getTime()/1000),
+    // fill in secret for production code
+    }, JWT_SECRET)
+}
 
 User.plugin(passportLocalMongoose);
 
